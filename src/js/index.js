a0e6631749bfa2fa5599d7e035f7775d811bebf1
Cyleng <jacques.leng@gmail.com>
2021-05-20 02:03:57 -0500

    folder change

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
 const http = require('http');
 const app = express();
+
 const server = http.createServer(app);
+const IO = socketIO(server);
 
 const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname,'../pub');
 
 app.use(express.static(publicPath));
+
+
 
 server.listen(port,()=>{
     console.log(`server up on ${port}`);
